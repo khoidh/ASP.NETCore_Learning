cd017d44603c5dca8c5966ccb12f672ecb373a8f
khoidh <khoidh@huongvietgroup.com>
2021-08-17 15:49:50 +0700

    [Khoidh] Add controller, Install Bootstrap, Install Jquery

---
+++ MVCCoreApp/MVCCoreApp/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCoreApp.Controllers
+{
+    public class DefaultController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string Index(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return "Welcome to ASP.NET Core MVC";
+            else
+                return $"Hello, {id}! Welcome to ASP.NET Core MVC";
+        }
+    }
+}
