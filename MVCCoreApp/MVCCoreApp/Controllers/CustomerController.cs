022853ee42715c6baba6f1c95638d74645ba7c35
khoidh <khoidh@huongvietgroup.com>
2021-09-14 23:16:10 +0700

    Attribute-Based Routing Learning

---
+++ MVCCoreApp/MVCCoreApp/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCoreApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        [Route("khach-hang/vip")]
+        public string Vip()
+        {
+            return "Khách hàng vip";
+        }
+
+        //[Route("")]                           // Set default when link = "/"
+        [Route("khach-hang/normal/{id?}")]
+        public string Normal(int id)
+        {
+            return $"Khách hàng thường với id: {id}";
+        }
+    }
+}
